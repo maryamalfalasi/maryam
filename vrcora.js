2cbe26e199f6d6aeae2a8e7cd230875d37e9bc09
maryamalfalasi <maryamalfalasi@didi.ac.ae>
2020-12-13 22:58:44 +0400

    f

--- vrcora.js
+++ vrcora.js
@@ -1,16 +1,28 @@
-var scene = document.querySelector("a-scene");
-    var vid = document.getElementById("video");
+document.addEventListener("DOMContentLoaded", function(event) {
+  var scene = document.querySelector("a-scene");
+  var vid = document.getElementById("video");
+  var videoShere = document.getElementById("videoShere");
 
-    if (scene.hasLoaded) {
-      run();
+  if (scene.hasLoaded) {
+    run();
+  } else {
+    scene.addEventListener("loaded", run);
+  }
+
+  function run () {
+    if(AFRAME.utils.device.isMobile()) {
+      document.querySelector('#splash').style.display = 'flex';
+      document.querySelector('#splash').addEventListener('click', function () {
+        playVideo();
+        this.style.display = 'none';
+      })
     } else {
-      scene.addEventListener("loaded", run);
+        playVideo();
     }
+  }
 
-    function run () {
-        scene.querySelector(".a-enter-vr-button").addEventListener("click", function(e){
-            console.log("VR Mode entered");
-            this.style.display = "none";
-            vid.play();
-        }, false);
-    }
+  function playVideo () {
+    vid.play();
+    videoShere.components.material.material.map.image.play();
+  }
+})
